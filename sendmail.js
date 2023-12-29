3539409fe405b65af453024f49efd4da27bdaa7d
Nandini Jindal <nandinijindal010@gmail.com>
2023-12-29 13:55:30 +0530

    sendmail

---
+++ sendmail.js
@@ -0,0 +1,50 @@
+const nodemailer = require("nodemailer");
+const path = require("path");
+const hbs = require('nodemailer-express-handlebars');
+
+async function SendMail(email) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: "aryangoyal625@gmail.com",
+      pass: "rjxgtdhhjatkywrc"
+    }
+  });
+
+  const handlebarOptions = {
+    viewEngine: {
+      extName: ".handlebars",
+      partialsDir: path.resolve('./public'),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve('./public'),
+    extName: ".handlebars",
+  }
+  transporter.use('compile', hbs(handlebarOptions));
+
+  transporter.sendMail({
+    from: 'aryangoyal625@gmail.com',
+    to: email,
+    subject: 'JSCOP Inhouse Hackathon',
+    template: 'email',
+    context: {
+      title: 'JSCOP Inhouse Hackathon',
+      text: "Optica is a society dedicated to the advancement of optics and photonics. Members are likely professionals, researchers, and enthusiasts in the field of optics."
+    },
+    attachments: [
+      {
+        filename: 'optica.jpg',  // Change the filename to match your image file
+        path: path.resolve('./public/uploads/optica.jpg'), // Change the path to the actual path of your image
+        cid: 'unique@nodemailer.com'  // Use the same cid as referenced in your Handlebars template
+      }
+    ]
+
+  }, (err, info) => {
+    if (err) throw err;
+    console.log(`Mail sent to ${email}: ${info.response}`);
+  });
+}
+
+module.exports = {
+  SendMail
+}
